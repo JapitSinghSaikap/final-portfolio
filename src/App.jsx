ec50610109a3dc79c1ca87d4c0f7d8235c5e5157
Japit302Saikap <singhjap302@gmail.com>
2025-03-07 17:23:18 +0530

    Naming Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import Navbar from './components/navbar';
+// import Navbar from './components/Navbar';
+import Navbar from "../components/Navbar";
 import About from './components/About';
 // import Projects from './components/Projects';
 import Projects from "../components/projects";
-import Header from './components/header';
+import Header from './components/Header';
 import Contact from './components/Contacts';
-import Experience from "./components/experience";
-import Skills from "./components/skills";
+// import Experience from "./components/Experience";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
 // import Projects from "./components/Projects";
 
 const Particle = ({ x, y, size, delay }) => {
